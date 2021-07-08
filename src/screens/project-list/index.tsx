a65f49d0ab202a9ecd09148983cbdf941696530c
lalalawututu <514065488@qq.com>
2021-07-08 21:54:38 +0800

    chore:添加类型定义

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { SearchPanel } from "./search";
+import { List } from "./list";
+import { useState, useEffect } from "react";
+import { cleanObject, useMount, useDebounce } from "../../utils";
+import * as qs from "qs";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+
+  const debouncedParam = useDebounce(param, 2000);
+
+  const [list, setList] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [debouncedParam]);
+
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  });
+
+  return (
+    <div>
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List users={users} list={list} />
+    </div>
+  );
+};
