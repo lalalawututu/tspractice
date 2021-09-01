f7d6e94dd4f322113db3a651f853b130d6c3a697
lalalawututu <514065488@qq.com>
2021-09-01 21:03:52 +0800

    chore: add dev-tool

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { ProjectListScreen } from "./screens/project-list/index";
+import { LoginScreen } from "./screens/login/index";
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen />
+      {/* <ProjectListScreen /> */}
+      <LoginScreen />
     </div>
   );
 }
